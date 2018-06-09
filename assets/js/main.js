03f7066aec99cd06d272e6aefa50738211e51a5a
Nick Mangos <nick.mangos@gmail.com>
2018-06-09 22:26:23 +1200

    Clean Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,16 @@ jQuery('.image-container').children('img').each(function() {
 	$(this).attr("data-src",$(this).attr("src"));
 	$(this).removeAttr("src");
 });
+
+$("#my_form").submit(function(event){
+    event.preventDefault(); //prevent default action 
+    var form_data = $(this).serialize(); //Encode form elements for submission
+    $.ajax({
+    	method: 'POST',
+    	url: 'https://www.enformed.io/brq2ec8/',
+    	dataType: 'json',
+    	data: form_data,
+    	success: $("#my_form")[0].reset(),
+    	error: (err) => console.log(err)
+    });
+});
